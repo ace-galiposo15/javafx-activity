5ea9b832b59b21913907df8cb72e8707992da0ab
Galiposo <galiposoace0@gmail.com>
2025-04-03 14:35:27 +0800

    set Methods for traffic-light-view.fxml

--- src/main/java/aclcbukidnon/com/javafxactivity/controllers/TrafficLightController.java
+++ src/main/java/aclcbukidnon/com/javafxactivity/controllers/TrafficLightController.java
@@ -3,14 +3,24 @@ package aclcbukidnon.com.javafxactivity.controllers;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.util.Duration;
 
-import java.util.Timer;
 
 
 
 public class TrafficLightController {
+    @FXML
+    private Circle redLight;
+    @FXML
+    private Circle yellowLight;
+    @FXML
+    private Circle greenLight;
+
+    private Timeline timeline;
+
+
 
     private enum TrafficLightColor {
         STOP,
@@ -21,21 +31,38 @@ public class TrafficLightController {
 
     private TrafficLightColor currentColor = TrafficLightColor.STOP;
 
-    private Timeline timeline;
+    private TrafficLightColor getCurrentColor() {
+        return currentColor;
+    }
 
+    private void setCurrentColor(TrafficLightColor currentColor) {
+        this.currentColor = currentColor;
+    }
 
     @FXML
-    public void initialize(){
-        timeline = new Timeline(
+    private void initialize(){
+        Timeline timeline = new Timeline(
                 new KeyFrame(Duration.seconds(3), e -> onTimerChange())
         );
         timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.play();
     }
 
 
     ///  What happens if the time is up
-    public void onTimerChange() {
+    private void onTimerChange() {
+        switch (currentColor){
+            case STOP -> currentColor=TrafficLightColor.HOLD;
+            case HOLD -> currentColor=TrafficLightColor.GO;
+            case GO -> currentColor=TrafficLightColor.STOP;
 
+        }
+        updateLights();
+    }
+    private void updateLights() {
+        redLight.setFill(currentColor == TrafficLightColor.STOP ? Color.RED : Color.DARKGRAY);
+        yellowLight.setFill(currentColor == TrafficLightColor.HOLD ? Color.YELLOW : Color.DARKGRAY);
+        greenLight.setFill(currentColor == TrafficLightColor.GO ? Color.LIMEGREEN : Color.DARKGRAY);
     }
 
 }
