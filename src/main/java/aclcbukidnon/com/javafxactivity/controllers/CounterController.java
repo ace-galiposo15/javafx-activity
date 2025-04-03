35fe57c2a31ee56a69f004ba08ab6a4d02ab115d
Galiposo <galiposoace0@gmail.com>
2025-04-03 14:11:46 +0800

    set Methods for counter-view.fxml

--- src/main/java/aclcbukidnon/com/javafxactivity/controllers/CounterController.java
+++ src/main/java/aclcbukidnon/com/javafxactivity/controllers/CounterController.java
@@ -1,4 +1,31 @@
 package aclcbukidnon.com.javafxactivity.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class CounterController {
+    private int count =0;
+    @FXML
+    private Label LabelCounter;
+
+    @FXML
+    private void initialize() {
+        updateCounter();
+    }
+
+    @FXML
+    private void onClickIncrement() {
+        count++;
+        updateCounter();
+    }
+
+    @FXML
+    private void onClickDicrement() {
+        count--;
+        updateCounter();
+    }
+
+    private void updateCounter() {
+        LabelCounter.setText(String.valueOf(count));
+    }
 }
